e37dd8b545eb556594fa94a99b619cdb632a8cb4
Maria Golovatenko <pikch4y@yandex.com>
2023-08-22 15:25:25 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const limiter = require('./middlewares/rate-limit');
 const errorHandler = require('./middlewares/error-handler');
@@ -12,6 +13,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, DB_URL } = process.env;
 
 const app = express();
+app.use(cors({
+  origin: ['http://localhost:3001', 'https://explore-movies.nomoredomainsicu.ru'],
+  credentials: true,
+}));
 
 app.use(requestLogger);
 app.use(limiter);
